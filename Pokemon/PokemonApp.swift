70547d72aacca3ce116ae0faee8f49586794d040
Etienne Vautherin <etienne@vautherin.com>
2024-02-20 12:17:51 +0100

    Initial Commit

---
+++ Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by Etienne Vautherin on 20/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
