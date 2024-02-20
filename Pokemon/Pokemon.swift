0623f32f86401e751b4cc955e7cd4d706eb45727
Etienne Vautherin <etienne@vautherin.com>
2024-02-20 15:47:59 +0100

    Swift.Error

--- Pokemon/Pokemon.swift
+++ Pokemon/Pokemon.swift
@@ -12,17 +12,17 @@ class Pokemon: Codable {
     let id: Int
     let name: String
     
-    enum PokemonError: Error {
+    enum Error: Swift.Error {
         case url
         case request
     }
     
     static func getPokemon(urlString: String) async throws -> Pokemon {
-        guard let url = URL(string: urlString) else { throw PokemonError.url }
+        guard let url = URL(string: urlString) else { throw Error.url }
         
         let (data, response) = try await URLSession.shared.data(from: url)
         
-        guard (response as? HTTPURLResponse)?.statusCode == 200 else { throw PokemonError.request }
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else { throw Error.request }
         
         let pokemon = try JSONDecoder().decode(Pokemon.self, from: data)
         return pokemon
